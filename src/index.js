888fad236b924c1804b0adc75b6e773e7aa093c4
Olexandr <alexandr90h@gmail.com>
2020-10-29 23:05:25 +0300

    set plagin

--- src/index.js
+++ src/index.js
@@ -1,16 +1,38 @@
 import './styles.scss';
 import listCountry from "./templates/list-countrys.hbs";
 import cardCountTempl from "./templates/card-countrys.hbs";
+import '@pnotify/core/dist/BrightTheme.css';
+// import { alert, defaultModules } from 'node_modules/@pnotify/core/dist/PNotify.js';
+const PNotify = require('@pnotify/core');
+const debounce = require('lodash.debounce');
+const {defaults} = require('@pnotify/core');
+// defaults.styling = 'material';
+defaults.sticker= false;
+// defaults.mode = 'light';
+defaults.delay = '1000';
+// defaults.stack = {dir1: 'down',
+//   dir2: 'left',
+//   firstpos1: 25,
+//   firstpos2: 25,
+//   spacing1: 36,
+//   spacing2: 36,
+//   push: 'bottom',
+//   context: document.querySelector('result-box')};
+//     mode: 'light',
+//     maxTextHeight: null,
+//     sticker: false,
+//     delay: 1000
+
 const refs = {
     inputTextElem: document.querySelector(".input-js"),
     resultBoxElem: document.querySelector(".result-box"),
     listItemCountElem: document.querySelector(".list-item"),
 }
-refs.inputTextElem.addEventListener('input', getCountName);
+refs.inputTextElem.addEventListener('input', debounce(getCountName,500));
 function getCountName() {
     let charNameCountr = refs.inputTextElem.value;
     if (charNameCountr.length < 2)
-    { return clearListBox(); }
+    { return PNotify.alert('Too many matches found. Please enter a more specific query!')}
     fetch(`https://restcountries.eu/rest/v2/name/${charNameCountr}`).then(response => {
         return response.json();
     }).then((arr) => {
@@ -25,7 +47,6 @@ function renderListCount(arr) {
     refs.resultBoxElem.innerHTML = listCountry({ names });
 }
 function renderCartCount(arr) {
-    console.log('renderCartCount');
 const [{ name, flag, population, capital}] = arr;
 refs.resultBoxElem.innerHTML = cardCountTempl({name,flag, population,capital});
 }
