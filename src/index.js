08cc54875e4f637d4a1a8da2a0e3b36694e93c3c
Olexandr <63474923+alexandr90h@users.noreply.github.com>
2020-10-29 16:28:32 +0200

    29.10.2020-1

--- src/index.js
+++ src/index.js
@@ -1,32 +1,34 @@
 import './styles.scss';
 import listCountry from "./templates/list-countrys.hbs";
+import cardCountTempl from "./templates/card-countrys.hbs";
 const refs = {
     inputTextElem: document.querySelector(".input-js"),
     resultBoxElem: document.querySelector(".result-box"),
-   
+    listItemCountElem: document.querySelector(".list-item"),
 }
-// let COUNTRYS_NAME=inpu
 refs.inputTextElem.addEventListener('input', getCountName);
 function getCountName() {
     let charNameCountr = refs.inputTextElem.value;
     if (charNameCountr.length < 2)
-    {return console.log('error'); }
+    { return clearListBox(); }
     fetch(`https://restcountries.eu/rest/v2/name/${charNameCountr}`).then(response => {
         return response.json();
     }).then((arr) => {
-        // const listItemElem=document.querySelector('.list-item');
-        // refs.resultBoxElem.removeChild(listItemElem);
+        if (arr.length === 1) {
+            return renderCartCount(arr);
+        }
         renderListCount(arr);
     }).catch(error=>console.log(error));
 }
 function renderListCount(arr) {
-    // refs.resultBoxElem.removeChild(refs.listItemElem);
-    const listItemElem = document.querySelector('.list-item');
-    //  console.log(listItemElem);
-    if (listItemElem != null) { listItemElem.remove(); console.log('del');}
-            const names = arr.map(({ name }) => name);
-    refs.resultBoxElem.insertAdjacentHTML("beforeend", arr.map(listCountry).join(''));
-    
-    console.log(arr);
-    // console.log(arr.map(listCountry).join(''));
+    const names = arr.map(({ name }) => name);
+    refs.resultBoxElem.innerHTML = listCountry({ names });
+}
+function renderCartCount(arr) {
+    console.log('renderCartCount');
+const [{ name, flag, population, capital}] = arr;
+refs.resultBoxElem.innerHTML = cardCountTempl({name,flag, population,capital});
+}
+function clearListBox() {
+    refs.resultBoxElem.removeChild(refs.listItemCountElem)
 }
