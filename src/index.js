07ece0378c2993b9a1e7cf7e48bbee07216cdbfb
Olexandr <63474923+alexandr90h@users.noreply.github.com>
2020-10-30 17:45:09 +0200

    30.10.2020-1

--- src/index.js
+++ src/index.js
@@ -2,42 +2,37 @@ import './styles.scss';
 import listCountry from "./templates/list-countrys.hbs";
 import cardCountTempl from "./templates/card-countrys.hbs";
 import '@pnotify/core/dist/BrightTheme.css';
-// import { alert, defaultModules } from 'node_modules/@pnotify/core/dist/PNotify.js';
-const PNotify = require('@pnotify/core');
-const debounce = require('lodash.debounce');
-const {defaults} = require('@pnotify/core');
-// defaults.styling = 'material';
-defaults.sticker= false;
-// defaults.mode = 'light';
-defaults.delay = '1000';
-// defaults.stack = {dir1: 'down',
-//   dir2: 'left',
-//   firstpos1: 25,
-//   firstpos2: 25,
-//   spacing1: 36,
-//   spacing2: 36,
-//   push: 'bottom',
-//   context: document.querySelector('result-box')};
-//     mode: 'light',
-//     maxTextHeight: null,
-//     sticker: false,
-//     delay: 1000
-
+import {alert, Stack} from '@pnotify/core';
 const refs = {
     inputTextElem: document.querySelector(".input-js"),
     resultBoxElem: document.querySelector(".result-box"),
     listItemCountElem: document.querySelector(".list-item"),
+    cardCountElem: document.querySelector(".card-section"),
 }
+
+const PNotify = require('@pnotify/core');
+const debounce = require('lodash.debounce');
+const {defaults} = require('@pnotify/core');
+defaults.sticker= false;
+defaults.delay = '1000';
+defaults.maxTextHeight = null;
+defaults.stack = new Stack({
+    dir1: 'up',
+    firstpos1: 10,
+    maxOpen: 1,
+    context: refs.resultBoxElem,
+});
 refs.inputTextElem.addEventListener('input', debounce(getCountName,500));
 function getCountName() {
     let charNameCountr = refs.inputTextElem.value;
-    if (charNameCountr.length < 2)
-    { return PNotify.alert('Too many matches found. Please enter a more specific query!')}
     fetch(`https://restcountries.eu/rest/v2/name/${charNameCountr}`).then(response => {
         return response.json();
     }).then((arr) => {
         if (arr.length === 1) {
             return renderCartCount(arr);
+        }
+        else if (arr.length > 10) {
+            return notificationListCount();
         }
         renderListCount(arr);
     }).catch(error=>console.log(error));
@@ -47,9 +42,16 @@ function renderListCount(arr) {
     refs.resultBoxElem.innerHTML = listCountry({ names });
 }
 function renderCartCount(arr) {
-const [{ name, flag, population, capital}] = arr;
-refs.resultBoxElem.innerHTML = cardCountTempl({name,flag, population,capital});
+    const [{ name, flag, population, capital, languages }] = arr;
+refs.resultBoxElem.innerHTML = cardCountTempl({name,flag, population, capital, languages});
 }
-function clearListBox() {
-    refs.resultBoxElem.removeChild(refs.listItemCountElem)
+function notificationListCount() {
+    if (refs.listItemCountElem != null) {
+        refs.resultBoxElem.removeChild(refs.listItemCountElem);
+    }
+    if (refs.cardCountElem != null) {
+        console.log("ergbebte");
+        refs.resultBoxElem.removeChild(refs.cardCountElem);
+    }
+    alert('Too many matches found. Please enter a more specific query!');
 }
