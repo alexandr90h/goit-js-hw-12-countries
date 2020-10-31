4f7b1cce57851947ffd37d2095b82a23b31693ba
Olexandr <alexandr90h@gmail.com>
2020-10-31 23:10:37 +0300

    31.10.2020-2

--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ import './styles.scss';
 import listCountry from "./templates/list-countrys.hbs";
 import cardCountTempl from "./templates/card-countrys.hbs";
 import '@pnotify/core/dist/BrightTheme.css';
-import {alert, Stack} from '@pnotify/core';
+import autoprefixer from 'autoprefixer';
+import {info, Stack} from '@pnotify/core';
+// import { Stats } from 'webpack';
 const refs = {
     inputTextElem: document.querySelector(".input-js"),
     resultBoxElem: document.querySelector(".result-box"),
@@ -12,32 +14,20 @@ const refs = {
 
 const PNotify = require('@pnotify/core');
 const debounce = require('lodash.debounce');
-const {defaults} = require('@pnotify/core');
-defaults.sticker= false;
-defaults.delay = '1000';
-defaults.maxTextHeight = null;
-defaults.stack = new Stack({
-    dir1: 'up',
-    firstpos1: 100,
-    maxOpen: 1,
-    context: refs.resultBoxElem,
-});
-refs.inputTextElem.addEventListener('input', debounce(getCountName(),500));
+refs.inputTextElem.addEventListener('input', debounce(getCountName,500));
 function getCountName() {
-    let tempArr = [];
     let charNameCountr = refs.inputTextElem.value;
     fetch(`https://restcountries.eu/rest/v2/name/${charNameCountr}`).then(response => {
         return response.json();
     }).then((arr) => {
-        tempArr = arr;
-        if (arr.length === 1) {
-            return renderCartCount(arr);
+        if (arr.length ===1) {
+           return renderCartCount(arr);
         }
-        else if (arr.length > 10 || tempArr!=arr) {
-            return notificationListCount();
+        else if (arr.length > 10) {
+           return notificationListCount();
         }
         renderListCount(arr);
-    }).catch(error=>console.log(error));
+    }).catch();
 }
 function renderListCount(arr) {
     const names = arr.map(({ name }) => name);
@@ -45,15 +35,24 @@ function renderListCount(arr) {
 }
 function renderCartCount(arr) {
     const [{ name, flag, population, capital, languages }] = arr;
-refs.resultBoxElem.innerHTML = cardCountTempl({name,flag, population, capital, languages});
+    refs.resultBoxElem.innerHTML = cardCountTempl({ name, flag, population, capital, languages });
 }
 function notificationListCount() {
-    if (refs.listItemCountElem != null) {
-        refs.resultBoxElem.removeChild(refs.listItemCountElem);
-    }
-    else if (refs.cardCountElem != null) {
-        console.log(refs.cardCountElem);
-        refs.resultBoxElem.removeChild(refs.cardCountElem);
-    }
-    alert('Too many matches found. Please enter a more specific query!');
+    refs.resultBoxElem.innerHTML = '';
+    info({
+      sticker: false,
+        delay: '1000',
+        maxOpen: 1,
+        maxTextHeight: null,
+        closer: false,
+        closerHover: true,
+        title: false,
+        text: 'Too many matches found. Please enter a more specific query!',
+        stack: new Stack({
+            dir1: 'down',
+            // dir2: 'right',
+            modal: true,
+            context: refs.resultBoxElem,
+       })
+    })
 }
